aef41c71a52f7c4db0fd4c34fca1000b028dcea6
Aliia <aliiasaidillaeva@gmail.com>
2022-06-26 19:49:36 +0600

    fixed error with access modifier

--- Sources/KingsCode/KingsCode.swift
+++ Sources/KingsCode/KingsCode.swift
@@ -1,16 +1,16 @@
 import UIKit
 
-extension UIView {
+public extension UIView {
     
     @resultBuilder
-    public struct ConstraintsBuilder {
+    struct ConstraintsBuilder {
         static func buildBlock(_ components: NSLayoutConstraint...) -> [NSLayoutConstraint] {
             components
         }
     }
     
     @discardableResult
-    public func createConstraints(@ConstraintsBuilder constraints: (() -> [NSLayoutConstraint])) -> [NSLayoutConstraint] {
+    func createConstraints(@ConstraintsBuilder constraints: (() -> [NSLayoutConstraint])) -> [NSLayoutConstraint] {
         constraints()
     }
 }
